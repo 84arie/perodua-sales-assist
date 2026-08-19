19b4e7aefa9c11650c86b3f1666a3decee22523e
84arie <84arie@gmail.com>
2026-08-19 09:05:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="psa-v3-2-id-fix";
+const CACHE="psa-v3-3-login-every-open";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
